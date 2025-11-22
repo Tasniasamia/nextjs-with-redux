571b185d70baeebe4e0205c9b0c97132d5076f26
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-11-22 13:03:46 +0600

    first

---
+++ redux/features/counterController.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+interface CounterState {
+  value: number;
+}
+const initialState: CounterState = { value: 0 };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+        state.value = state.value + 1;
+      
+    },
+    decrement(state) {
+      if (state.value === 0) {
+        return;
+      }
+        state.value = state.value - 1;
+      
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+
+export const selectCount = (state: any) => state.counter.value;
+
+export default counterSlice.reducer;
